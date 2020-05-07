27462727f7d2b51895e474ea8dae67b476671201
= <=>
2020-05-07 17:30:38 +0200

    snake moving

--- snake.js
+++ snake.js
@@ -1,3 +1,13 @@
+var snake=[
+    {
+        x:1,
+        y:0
+    },
+    {
+        x:0,
+        y:0
+    }
+]
 const size=20;
 const food={
     x:0,
@@ -7,4 +17,15 @@ function placeFood()
 {
     food.x = Math.floor(Math.random() * size);
     food.y = Math.floor(Math.random() * size);
+}
+function snakeMove ()
+{
+    const head=snake[0]
+    snake.unshift(
+        {
+           x:head.x+1,
+           y:0 
+        }
+    );
+    snake.pop()
 }
