a209de11a071fe2fa4f314280419f1ec1026679c
Michał Niedźwiedzki <michal@epsi.pl>
2020-05-08 09:51:03 +0200

    Code cleanup, added comments, prepared for food check

--- snake.js
+++ snake.js
@@ -1,31 +1,24 @@
-var snake=[
-    {
-        x:1,
-        y:0
-    },
-    {
-        x:0,
-        y:0
-    }
-]
-const size=20;
-const food={
-    x:0,
-    y:0
-}
-function placeFood()
-{
+const snake = [
+    { x: 1, y: 0 },
+    { x: 0, y: 0 },
+];
+const size = 20;
+const food = { x: 0, y: 0 };
+
+function placeFood() {
     food.x = Math.floor(Math.random() * size);
     food.y = Math.floor(Math.random() * size);
 }
-function snakeMove ()
-{
-    const head=snake[0]
-    snake.unshift(
-        {
-           x:head.x+1,
-           y:0 
-        }
-    );
-    snake.pop()
+
+function snakeMove() {
+    const oldHead = snake[0]
+
+    // add new head to snake
+    const newHead = { x: oldHead.x + 1, y: 0 };
+    snake.unshift(newHead);
+
+    // check if head not on food, then pop the tail
+    if (true) {
+        snake.pop();
+    }
 }
