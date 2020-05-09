c9d3ef9c244ed7615344539f2e6785820d6febb9
Tolek Rudnicki <tolek@epsi.pl>
2020-05-09 18:26:00 +0200

    walking on y's and not poping the tail at fiid

--- snake.js
+++ snake.js
@@ -10,15 +10,15 @@ function placeFood() {
     food.y = Math.floor(Math.random() * size);
 }
 
-function snakeMove() {
+function snakeMove(mx,my) {
     const oldHead = snake[0]
 
     // add new head to snake
-    const newHead = { x: oldHead.x + 1, y: 0 };
+    const newHead = { x: oldHead.x + mx, y: oldHead.y + my };
     snake.unshift(newHead);
 
     // check if head not on food, then pop the tail
-    if (true) {
+    if (newHead.x!=food.x && newHead.y!=food.y) {
         snake.pop();
     }
 }
