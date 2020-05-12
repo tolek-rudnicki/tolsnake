b88cdbbd5ab9b4d11e7a19be7341814819a70253
Michał Niedźwiedzki <michal@epsi.pl>
2020-05-12 10:34:34 +0200

    Refactored, extracted to a separate file

---
+++ board.js
@@ -0,0 +1,79 @@
+function installHandlers() {
+    // snake direction controls
+    window.addEventListener("keydown", function (event) {
+        switch (event.keyCode) {
+            case 39: document.getElementById("direction").value = "right"; break;
+            case 40: document.getElementById("direction").value = "down"; break;
+            case 37: document.getElementById("direction").value = "left"; break;
+            case 38: document.getElementById("direction").value = "up"; break;
+        }
+    });
+
+    // game run control
+    document.getElementById("run").addEventListener("change", function () {
+        if (this.checked) {
+            executeGameTick();
+        }
+    });
+
+    // food placement control
+    document.getElementById("randomize-food").addEventListener("click", function () {
+        placeFood();
+        console.log(food);
+        drawBoard(size, snake, food);
+        updateGameStats()
+    });
+}
+
+function drawBoard(size, snake, food) {
+    const board = document.getElementById("board");
+    board.innerHTML = "";
+    for (let y = 0; y < size; ++y) {
+        const row = document.createElement("div");
+        board.appendChild(row);
+        for (let x = 0; x < size; ++x) {
+        const field = document.createElement("span");
+        field.id = "field-" + x + "-" + y;
+        row.appendChild(field);
+        }
+    }
+    // draw food
+    document.getElementById("field-" + food.x + "-" + food.y).className = "food";
+    // draw snake
+    for (let i = 0; i < snake.length; ++i) {
+        const body = snake[i];
+        const fieldId = "field-" + body.x + "-" + body.y;
+        console.log(fieldId);
+        document.getElementById(fieldId).className = (i === 0 ? "head" : "tail");
+    }
+}
+
+function updateGameStats() {
+    document.getElementById("length").value = snake.length;
+    document.getElementById("snake").value = snake.map(function ({x, y}) { return x + ":" + y }).join(", ");
+    document.getElementById("food").value = food.x + ":" + food.y;
+}
+
+function executeGameTick() {
+    if (!document.getElementById("run").checked) {
+        return;
+    }
+
+    // move snake
+    switch (document.getElementById("direction").value) {
+        case "right": snakeMove(1, 0); break;
+        case "down": snakeMove(0, 1); break;
+        case "left": snakeMove(-1, 0); break;
+        case "up": snakeMove(0, -1); break;
+    }
+    console.log(snake);
+
+    // refresh board and stats
+    drawBoard(size, snake, food);
+    updateGameStats();
+
+    // prepare execution of next game tick
+    const sleep = 1000 / parseFloat(document.getElementById("pace").value);
+    console.log("Next tick in " + sleep + "ms");
+    setTimeout(executeGameTick, sleep);
+}
